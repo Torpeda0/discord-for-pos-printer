d08bd0e86bc9d6997cd5324bfcc4a87e6526c5d4
Torpeda <jankowskis422@gmail.com>
2021-12-21 19:02:47 +0100

    dodano sprawdzanie statusu papieru

--- main.py
+++ main.py
@@ -26,14 +26,15 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if (message.channel.id == channel_id):
-        wiadomosc = message.content.replace("\n", " ")
-        if len(wiadomosc) > 128:
-            print("Message detected (Shortened to 128 characters.) : " + wiadomosc[:128] + "    (" "time : " + str(message.created_at)[:-7] + ")")
-            printer.text("@" + message.author.name + "#" + message.author.discriminator + "   " + str(message.created_at)[:-7] +" : \n")
-            printer.text(wiadomosc[:128] + "\n")
-        else: 
-            print("Message detected : " + wiadomosc + "    (" "Time: " + str(message.created_at)[:-7] + ")")
-            printer.text("@" + message.author.name + "#" + message.author.discriminator + "   " + str(message.created_at)[:-7] +" : \n")
-            printer.text(wiadomosc + "\n")
+        if printer.paper_status != 0:
+            wiadomosc = message.content.replace("\n", " ")
+            if len(wiadomosc) > 128:
+                print("Message detected (Shortened to 128 characters.) : " + wiadomosc[:128] + "    (" "time : " + str(message.created_at)[:-7] + ")")
+                printer.text("@" + message.author.name + "#" + message.author.discriminator + "   " + str(message.created_at)[:-7] +" : \n")
+                printer.text(wiadomosc[:128] + "\n")
+            else: 
+                print("Message detected : " + wiadomosc + "    (" "Time: " + str(message.created_at)[:-7] + ")")
+                printer.text("@" + message.author.name + "#" + message.author.discriminator + "   " + str(message.created_at)[:-7] +" : \n")
+                printer.text(wiadomosc + "\n")
 
 client.run(token)
